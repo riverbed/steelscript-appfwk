b3fb868fec073f4046d5494523aa46fca8e656df
Mike Garabedian <mike.garabedian@riverbed.com>
2014-05-14 11:40:24 -0400

    Clean up

--- steelscript/appfwk/commands/__init__.py
+++
@@ -1,7 +0,0 @@
-#from steelscript.commands.steel import BaseCommand
-#
-#
-#class Command(BaseCommand):
-#    keyword = 'appfwk'
-#    help = 'App Framework subcommands'
-#    submodule = 'steelscript.appfwk.commands'
