7ab356f0e51b8b9ad1a7a4797eabf3872bfd6762
Mike Garabedian <mike.garabedian@riverbed.com>
2014-05-17 11:38:30 -0400

    Update headers, 2014

--- steelscript/appfwk/apps/devices/serializers.py
+++ steelscript/appfwk/apps/devices/serializers.py
@@ -1,3 +1,10 @@
+# Copyright (c) 2014 Riverbed Technology, Inc.
+#
+# This software is licensed under the terms and conditions of the MIT License
+# accompanying the software ("License").  This software is distributed "AS IS"
+# as set forth in the License.
+
+
 from rest_framework import serializers
 
 from steelscript.appfwk.apps.devices.models import Device
