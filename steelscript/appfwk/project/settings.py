c13bb7b249a3e49c3bd445419d2937480bb73b87
Mike Garabedian <mike.garabedian@riverbed.com>
2014-05-19 15:16:07 -0400

    Fix admin branding

--- steelscript/appfwk/project/settings.py
+++ steelscript/appfwk/project/settings.py
@@ -113,13 +113,13 @@ TEMPLATE_DIRS = (
     # or "C:/www/django/templates".
     # Always use forward slashes, even on Windows.
     # Don't forget to use absolute paths, not relative paths.
-    os.path.join(PROJECT_ROOT, 'templates')
+    os.path.join(PROJECT_ROOT, 'templates'),
 )
 
 # List of callables that know how to import templates from various sources.
 TEMPLATE_LOADERS = (
-    'django.template.loaders.app_directories.Loader',
     'django.template.loaders.filesystem.Loader',
+    'django.template.loaders.app_directories.Loader',
     'django.template.loaders.eggs.Loader',
 )
 
