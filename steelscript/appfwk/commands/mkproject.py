93aea4d747522e5d82340f95a8194d367c7eb6d9
Mike Garabedian <mike.garabedian@riverbed.com>
2014-06-25 17:05:25 -0400

    Correct import statement

--- steelscript/appfwk/commands/mkproject.py
+++ steelscript/appfwk/commands/mkproject.py
@@ -7,7 +7,7 @@
 import os
 from random import choice
 
-from steelscript.common import link_pkg_dir, link_pkg_files
+from steelscript.common.utils import link_pkg_dir, link_pkg_files
 from steelscript.commands.steel import (BaseCommand, prompt, console, debug,
                                         shell, check_git, ShellFailed)
 
