0a28332c29b5c5886900386fc0493fe02cad1900
Mike Garabedian <mike.garabedian@riverbed.com>
2016-11-01 11:43:23 -0400

    Add extra templatetags filter that should have been committed earlier

---
+++ steelscript/appfwk/apps/report/templatetags/add_attrs.py
@@ -0,0 +1,32 @@
+# Copyright (c) 2016 Riverbed Technology, Inc.
+#
+# This software is licensed under the terms and conditions of the MIT License
+# accompanying the software ("License").  This software is distributed "AS IS"
+# as set forth in the License.
+
+
+# ref:
+# http://vanderwijk.info/blog/adding-css-classes-formfields-in-django-templates/
+
+from django import template
+register = template.Library()
+
+
+@register.filter(name='addcss')
+def addcss(field, css):
+    return field.as_widget(attrs={"class": css})
+
+
+@register.filter(name='add_attributes')
+def add_attributes(field, css):
+    attrs = {}
+    definition = css.split(',')
+
+    for d in definition:
+        if ':' not in d:
+            attrs['class'] = d
+        else:
+            t, v = d.split(':')
+            attrs[t] = v
+
+    return field.as_widget(attrs=attrs)
