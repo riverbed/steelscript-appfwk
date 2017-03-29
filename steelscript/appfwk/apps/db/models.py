ae6efbfd597a8d870cd916595fe792c9e18df3bc
wenxuan guo <wguo@riverbed.com>
2017-03-29 16:57:51 -0400

    Rm comments

--- steelscript/appfwk/apps/db/models.py
+++ steelscript/appfwk/apps/db/models.py
@@ -13,23 +13,16 @@ class ExistingIntervals(models.Model):
     """Store the existing time intervals in db for each table and
     a set of criteria fields (represented by the table_handle field).
     """
-    # Plugin name
     namespace = models.CharField(max_length=20)
 
-    # Source report module name
     sourcefile = models.CharField(max_length=200)
 
-    # Table name
     table = models.CharField(max_length=50)
 
-    # Criteria fields
     criteria = PickledObjectField(null=True)
 
-    # Time series data source table handle
     table_handle = models.CharField(max_length=100, default="")
 
-    # Existing data intervals
     intervals = PickledObjectField(null=True)
 
-    # timezone info
     tzinfo = PickledObjectField()
